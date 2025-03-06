58986d2e4c5c67ae5dbd069a6e22f64cecf2d552
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2025-03-06 11:45:16 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/luan/course/entities/User.java
+++ src/main/java/br/com/luan/course/entities/User.java
@@ -1,10 +1,17 @@
 package br.com.luan.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User  implements Serializable {
-    private Long id;
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
      private String name;
      private String email;
      private String phone;
