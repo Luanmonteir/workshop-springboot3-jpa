666a66ae96bbff6a8acbfa1bc4774354614f418c
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2025-03-11 12:21:04 -0300

    Service layer, component registration

--- src/main/java/br/com/luan/course/repositories/UserRepository.java
+++ src/main/java/br/com/luan/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package br.com.luan.course.repositories;
 
 import br.com.luan.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
